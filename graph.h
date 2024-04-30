3d6b0a37d1c3020d10eab7c91d9573224a380a25
Daniel Rivas <drivas8891@gmail.com>
2024-04-30 11:33:16 -0700

    Finish Bellman Ford algorithm implementation

--- graph.h
+++ graph.h
@@ -2,6 +2,7 @@
 #define GRAPH_H
 
 #include <iostream>
+#include <limits>
 #include <vector>
 using namespace std;
 
@@ -18,6 +19,40 @@ struct Graph {
 		edges.push_back(edge);
 	}
 
+	void bellmanFord(int src) {
+		vector<int> distance(V, INT_MAX);
+		distance[src] = 0;
+
+		// Relax all edges V-1 times
+		for (int i = 1; i < V - 1; ++i) {
+			for (const Edge& edge : edges) {
+				int u = edge.source;
+				int v = edge.destination;
+				int weight = edge.weight;
+
+				if (distance[u] != INT_MAX && distance[u] + weight < distance[v]) {
+					distance[v] = distance[u] + weight;
+				}
+			}
+		}
+
+		// Now we check for negative weight cycles
+		for (const Edge& edge : edges) {
+			int u = edge.source;
+			int v = edge.destination;
+			int weight = edge.weight;
+
+			if (distance[u] != INT_MAX && distance[u] + weight < distance[v]) {
+				cout << "Graph contains a negative weight cycle" << endl;
+				return;
+			}
+		}
+
+		cout << "Vertext Distance from Source" << endl;
+		for (int i = 0; i < V; ++i) {
+			cout << i << "\t\t" << distance[i] << endl;
+		}
+	}
 };
 
 #endif
